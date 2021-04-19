87b9c6857e9506727e224a8b79f989a93251fe01
Eli Zoller <zoller.eli@gmail.com>
2021-04-19 16:26:54 -0700

    add travis scripts for carapace

---
+++ travis_scripts_carapace.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Common checks to get run during the 'script' section in Travis.
+OUTPUT=0
+
+# Make OUTPUT equal return code if return code is not 0
+function checkReturn {
+  if [ $1 -ne 0 ]; then
+    OUTPUT=$1
+  fi
+}
+
+$SCRIPT_DIR/line_endings.sh $GITHUB_WORKSPACE/build_dir
+checkReturn $?
+
+phpcs --standard=Drupal --ignore=*.md,*-min.css,*.css --extensions=php,module,inc,install,test,profile,theme,css,info $GITHUB_WORKSPACE/build_dir
+checkReturn $?
+
+phpcs --standard=Drupal $GITHUB_WORKSPACE/build_dir/style/custom.css
+checkReturn $?
+
+phpcpd --suffix *.theme,*.module,*.inc,*.test,*.php $GITHUB_WORKSPACE/build_dir
+checkReturn $?
+
+exit $OUTPUT
